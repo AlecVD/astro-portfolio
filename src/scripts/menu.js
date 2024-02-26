23572c6c9a0333f5f2b692732e1cf4fa21c38743
alecvd <14879968+AlecVD@users.noreply.github.com>
2024-02-26 14:53:40 -0800

    use viewtransitions and update scripts

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,7 +1,9 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-    if(document.querySelector('.nav-links').style.display == 'block'){
-        document.querySelector('.nav-links').style.display = 'none'
-    }else{
-        document.querySelector('.nav-links').style.display = 'block'
+document.addEventListener("astro:page-load", () => {
+  document.querySelector(".hamburger").addEventListener("click", () => {
+    if (document.querySelector(".nav-links").style.display == "block") {
+      document.querySelector(".nav-links").style.display = "none";
+    } else {
+      document.querySelector(".nav-links").style.display = "block";
     }
   });
+});
